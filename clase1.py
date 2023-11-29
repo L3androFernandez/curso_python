1bcca07ee1ca3e464c73bf163b28381c844316d0
Leandro <fernandezzleandro36@gmail.com>
2023-11-29 17:49:19 -0300

    practicando

---
+++ clase1.py
@@ -0,0 +1,23 @@
+"""
+
+mi_nombre = "Coc"
+
+print(mi_nombre)"""
+
+"""Esto es una suma"""
+"""print(8+8)
+
+mi_nombre = "Leandro"
+
+mi_apellido = "Fernandez"
+
+
+
+"""
+""" notas_del_examen1 = int(input("Cual es la nota de tu primer parcial?"))
+
+notas_del_examen2 = int(input("Cual es la nota de tu segundo parcial?"))
+
+resultado = (notas_del_examen1 + notas_del_examen2) / 2
+
+print(resultado) """
